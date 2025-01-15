536b25767ada028c57cc6bba2d6666dcfeea71ee
AmirHossein Takhti <amirhosseintakhti.at@gmail.com>
2025-01-15 16:01:05 +0330

    commit

--- contentConditionally/app.js
+++ contentConditionally/app.js
@@ -1,16 +1,19 @@
 const app = Vue.createApp({
-    data(){
-        return{
-            goalValue:'',
-            goals:[]
-        };
-    },
-    methods:{
-        addGoal(){
+  data() {
+    return {
+      goalValue: "",
+      goals: [],
+    };
+  },
+  methods: {
+    addGoal() {
+      const trimmedGoal = this.goalValue.trim();
+      if (trimmedGoal) {
         this.goals.push(this.goalValue);
-        }
+        this.goalValue = "";
+      }
     },
-    
-})
+  },
+});
 
-app.mount('#app');
+app.mount("#app");
