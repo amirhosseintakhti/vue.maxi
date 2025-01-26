6d76ee9990b13cc2e0f50fc2e494813acf13d4ec
AmirHossein Takhti <amirhosseintakhti.at@gmail.com>
2025-01-27 00:04:15 +0330

    commit

--- monsterGame/app.js
+++ monsterGame/app.js
@@ -1,21 +1,33 @@
 Vue.createApp({
 
-    data(){
-        return{
-            monsterHealth:100,
-            playerHealth:100,
-
-        }
-    },
-    methods:{
-        attackMonster(){
-            const attackvalue = getRandomValue(5,12)
-            this.monsterHealth -= attackvalue;
-            this.attackPlayer();
+    data() {
+        return {
+          monsterHealth: 100,
+          playerHealth: 100,
+        };
+      },
+      methods: {
+        attackMonster() {
+          const attackValue = this.getRandomValue(5, 12);
+          this.monsterHealth = Math.max(this.monsterHealth - attackValue, 0);
+          this.attackPlayer();
         },
-        attackPlayer(){
-            const attackvalue = getRandomValue(5,12)
-            this.playerHealth -= attackvalue;
-        }
-    }
+        specialAttack() {
+          const attackValue = this.getRandomValue(10, 25);
+          this.monsterHealth = Math.max(this.monsterHealth - attackValue, 0);
+          this.attackPlayer();
+        },
+        healPlayer() {
+          const healValue = this.getRandomValue(8, 20);
+          this.playerHealth = Math.min(this.playerHealth + healValue, 100);
+          this.attackPlayer();
+        },
+        attackPlayer() {
+          const attackValue = this.getRandomValue(5, 15);
+          this.playerHealth = Math.max(this.playerHealth - attackValue, 0);
+        },
+        getRandomValue(min, max) {
+          return Math.floor(Math.random() * (max - min + 1)) + min;
+        },
+      },
 }).mount('#game')
