4752d0577c0c5ae0db92067c0e8a2fe66a104c07
AmirHossein Takhti <amirhosseintakhti.at@gmail.com>
2025-01-28 18:13:34 +0330

    commit

--- monsterGame/app.js
+++ monsterGame/app.js
@@ -1,42 +1,44 @@
 function getRandomValue(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
 Vue.createApp({
-
-    data() {
-        return {
-          monsterHealth: 100,
-          playerHealth: 100,
-        };
-      },
-      computed:{
-        monsterBarStyles(){
-            return { width: monsterHealth + '%' }
-        },
-        playerBarStyles(){
-            return { width: playerHealth + '%' }
-        }
-      },
-      methods: {
-        attackMonster() {
-          const attackValue = getRandomValue(5, 12);
-          this.monsterHealth -= attackValue; 
-          this.attackPlayer();
-        },
-        specialAttack() {
-          const attackValue = this.getRandomValue(10, 25);
-          this.monsterHealth = Math.max(this.monsterHealth - attackValue, 0);
-          this.attackPlayer();
-        },
-        healPlayer() {
-          const healValue = this.getRandomValue(8, 20);
-          this.playerHealth = Math.min(this.playerHealth + healValue, 100);
-          this.attackPlayer();
-        },
-        attackPlayer() {
-          const attackValue = getRandomValue(5, 15);
-          this.playerHealth -= attackValue ;
-        },
-      },
-}).mount('#game')
+  data() {
+    return {
+      monsterHealth: 100,
+      playerHealth: 100,
+    };
+  },
+  computed: {
+    monsterBarStyles() {
+      return {
+        width: this.monsterHealth + "%",
+        backgroundColor: this.monsterHealth > 50 ? "green" : "red",
+      };
+    },
+    playerBarStyles() {
+      return { width: this.playerHealth + "%" };
+    },
+  },
+  methods: {
+    attackMonster() {
+      const attackValue = getRandomValue(5, 12);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+    specialAttack() {
+      const attackValue = this.getRandomValue(10, 25);
+      this.monsterHealth = Math.max(this.monsterHealth - attackValue, 0);
+      this.attackPlayer();
+    },
+    healPlayer() {
+      const healValue = this.getRandomValue(8, 20);
+      this.playerHealth = Math.min(this.playerHealth + healValue, 100);
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      const attackValue = getRandomValue(5, 15);
+      this.playerHealth -= attackValue;
+    },
+  },
+}).mount("#game");
