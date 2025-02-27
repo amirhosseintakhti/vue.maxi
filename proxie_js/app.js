c7b89f716bd84f3803f09fd4e2bff4d7ef6a5c93
AmirHossein Takhti <amirhosseintakhti.at@gmail.com>
2025-02-28 03:03:03 +0330

    commit

--- proxie_js/app.js
+++ proxie_js/app.js
@@ -8,12 +8,16 @@ const handler = {
     set(target , key , value) {
         if( key === 'message'){
            target.longMessage = value + "amir";
+        }else if(key === 'longMessage'){
+            target.message += value;
         }
-        target.message = value;
+        target.message = "yogi"+value;
     },
 };
 
 
 const proxy = new Proxy( data , handler);
 proxy.message = "yogi";
+
 console.log(proxy.longMessage)
+console.log(proxy.message)
