04b582c589b12c9568fa891700f27fb70397707d
AmirHossein Takhti <amirhosseintakhti.at@gmail.com>
2025-04-16 08:29:33 +0330

    commit

--- introducingcomponents/app.js
+++ introducingcomponents/app.js
@@ -1,27 +1,52 @@
 const app = Vue.createApp({
   data() {
     return {
-      friends: [
-        {
+      friends:
+        [{
           id: "julia",
           phone: "09111763180",
           email: "juliajulia1@gmail.com",
           name: "julia jones",
         },
         {
-          id: "ali",
-          phone: "09111763170",
+            id: "ali",
+          phone: "09111763180",
           email: "aliali1@gmail.com",
           name: "ali takhti",
         },
-      ],
-      detailAreVisblity:false,
+    ],
+    //   detailAreVisblity: false,
     };
   },
-  methods:{
-    toggleDetails(){
-       this.detailAreVisblity = !this.detailAreVisblity;
-    }
-  }
+});
+
+app.component('friend-details', {
+  template: `
+     <li>
+                <h2>{{friend.name}}</h2>
+                <button @click="toggleDetails">{{detailAreVisblity?'hide':'show'}} list</button>
+                <ul v-if="detailAreVisblity">
+                    <li>{{friend.phone}}</li>
+                    <li>{{friend.email}}</li>
+                </ul>
+            </li>
+
+    `,
+  data() {
+    return {
+      friend: {
+        id: "julia",
+        phone: "09111763180",
+        email: "juliajulia1@gmail.com",
+        name: "julia jones",
+      },
+      detailAreVisblity: false,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailAreVisblity = !this.detailAreVisblity;
+    },
+  },
 });
 app.mount("#app");
