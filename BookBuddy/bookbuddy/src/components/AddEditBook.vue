8fde4f3f263426a95f773775b034ab90b623c91d
AmirHossein Takhti <amirhosseintakhti.at@gmail.com>
2025-08-15 16:23:06 +0330

    AddEditBook component develop

---
+++ BookBuddy/bookbuddy/src/components/AddEditBook.vue
@@ -0,0 +1,13 @@
+<template>
+    <form @submit.prevent="submitForm" class="add-edit-book">
+        <input v-model="title" type="text" placeholder="عنوان کتاب" required />
+        <input v-model="author" type="text" placeholder="نویسنده" required />
+        <button type="submit">افزودن کتاب</button>
+    </form>
+</template>
+
+<script>
+export default{
+    
+}
+</script>
