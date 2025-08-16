c6d61ba783c6101c55efd5b07e07ded3bfa3d46d
AmirHossein Takhti <amirhosseintakhti.at@gmail.com>
2025-08-17 01:21:16 +0330

    commit

---
+++ BookBuddy/bookbuddy/src/components/BookList.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <book-item
+      v-for="book in books"
+      :key="book.id"
+      :book="book"
+      @toggle-read="$emit('toggle-read', $event)"
+    />
+  </ul>
+</template>
+
+<script>
+import BookItem from "./BookItem.vue";
+export default {
+  components: { BookItem },
+  props: ["books"],
+  emits: ["toggle-read"],
+};
+</script>
+
+<style></style>
+ 
